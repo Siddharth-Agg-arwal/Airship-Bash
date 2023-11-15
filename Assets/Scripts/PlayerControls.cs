7d51a7a0983b2b15e402d166ed401ecb5e08c52c
Siddharth Aggarwal <saggarwal_be22@thapar.edu>
2023-11-16 01:38:48 +0530

    clamps

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,39 +2,56 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
 
     // [SerializeField] InputAction movement;
-    [SerializeField] float controlSpeed = 50f;
-    [SerializeField] float xRange = 25f;
+    [SerializeField] float controlSpeed = 20f;
+    [SerializeField] float xRange = 10f;
     [SerializeField] float yRange = 3.5f;
-    // Start is called before the first frame update
-
+    Vector3 currentPosition;
     // void OnEnable() {
     //     movement.Enable();    
     // }
 
     // void OnDisable() {
-    //     movement.Disable();
+    //     movement.Disable();    
     // }
-    
-    // Update is called once per frame
+    void Start() {
+    }
     void Update()
     {
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
+    void ProcessRotation(){
+        float pitch = 0f;
+        float yaw = 0f;
+        float roll = 0f;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessTranslation()
+    {
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
-        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
         float yOffset = yThrow * Time.deltaTime * controlSpeed;
         float rawYPos = transform.localPosition.y + yOffset;
-        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
-        
+
+        // currentPosition = GetComponent<Transform>().position;
+        float clampedXPos = Mathf.Clamp(rawXPos, rawXPos -xRange, rawXPos + xRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, rawYPos -yRange, rawYPos + yRange);
+
+        Debug.Log(clampedXPos);
+        Debug.Log(clampedYPos);
+
         transform.localPosition = new Vector3(
             clampedXPos,
             clampedYPos,
